e4331a70bddb2126654e8c6d6d632e3a3f20a67b
wiifm74 <wiifm74@users.noreply.github.com>
2024-04-29 19:35:20 +1000

    Add error code description to messaging

--- Modules/RapidChangeATC/Core/rcDebug.lua
+++ Modules/RapidChangeATC/Core/rcDebug.lua
@@ -3,6 +3,15 @@ rcDebug = {}
 local inst, _t, mt
 
 inst = mc.mcGetInstance( "rcDebug")
+
+package.path = string.format(
+	"%s;%s\\Modules\\?.lua;",
+	package.path,
+	mc.mcCntlGetMachDir(inst)
+)
+
+ec = require("mcErrorCheck")
+
 _t = rcDebug -- private access to original table
 rcDebug = {} -- proxy
 rcDebug.prototype = { rc = mc.MERROR_NOERROR, }
@@ -14,7 +23,7 @@ mt = { -- create metatable
 			
 			if (tonumber( v ) ~= mc.MERROR_NOERROR) then
 				
-				mc.mcCntlSetLastError(inst, string.format( "rcDebug Errorcode: %i", v  ) )
+				mc.mcCntlSetLastError(inst, string.format( "rcDebug Errorcode: %i - %s", v, ec[ v ] ) )
 			
 			end
 			
